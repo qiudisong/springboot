796a58d01603232633a91184f081895a9b9a785a
邱地松 <2111046603@qq.com>
2020-05-02 15:06:59 +0800

    整合logback，输出日志到文件

--- src/main/java/com/example/demo/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/config/GlobalExceptionHandler.java
@@ -1,26 +1,29 @@
 package com.example.demo.config;
 
 import com.example.demo.config.Exception.ParamException;
+import com.example.demo.util.ExceptionUtil;
 import com.example.demo.vo.ResultVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     /*通用异常处理方法*/
     @ExceptionHandler(Throwable.class)
     @ResponseBody
-    public ResultVo error(Throwable t){
-        t.printStackTrace();
+    public ResultVo error(Throwable t) {
+        log.error(ExceptionUtil.getMessage(t));
         return ResultVo.unknownError();
     }
 
     /*自定义异常处理方法*/
     @ExceptionHandler(ParamException.class)
     @ResponseBody
-    public ResultVo error(ParamException e){
-        e.printStackTrace();
+    public ResultVo error(ParamException e) {
+        log.error(ExceptionUtil.getMessage(e));
         return ResultVo.paramError(e.getMessage());
     }
 }
---
+++ src/main/java/com/example/demo/util/ExceptionUtil.java
@@ -0,0 +1,28 @@
+package com.example.demo.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+@Slf4j
+public class ExceptionUtil {
+
+    /**
+     * 打印异常信息
+     */
+    public static String getMessage(Throwable t) {
+        String swStr = null;
+        try (StringWriter sw = new StringWriter(); PrintWriter pw = new PrintWriter(sw)) {
+            t.printStackTrace(pw);
+            pw.flush();
+            sw.flush();
+            swStr = sw.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+        return swStr;
+    }
+}
