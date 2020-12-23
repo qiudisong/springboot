810c6387ce186b8d6dee6a7575b078c3c76713bb
邱地松 <qiudisong@ewell.cc>
2020-12-23 15:47:42 +0800

    使用flyway管理数据库

---
+++ src/main/resources/db/migration/V1_0_0__EXAMPLE_DATABASE.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `example` (
+  `id` int(11) DEFAULT NULL,
+  `aa` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `bb` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
