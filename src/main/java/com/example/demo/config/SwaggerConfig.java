1e5330126e097f36d51af26dc1b35eeea104548b
邱地松 <qiudisong@ewell.cc>
2021-01-27 14:40:22 +0800

    swagger基本信息修改

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import io.swagger.models.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -28,8 +27,8 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("springboot利用swagger构建api文档")
-                .description("简单优雅的restfun风格，https://www.baidu.com")
-                .termsOfServiceUrl("https://www.baidu.com")
+                .description("http://127.0.0.1:8080/demo/swagger-ui.html")
+                .termsOfServiceUrl("服务条款暂无")
                 .version("1.0")
                 .build();
 
